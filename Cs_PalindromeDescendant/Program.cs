5d6a210189834058661b5c117deec2fb3f7fc8d7
triptychlux <kacper-94@wp.eu>
2023-01-04 20:39:43 +0100

    first commit

---
+++ Cs_PalindromeDescendant/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cs_PalindromeDescendant
+{
+    class Program
+    {
+        public static bool PalindromeDescendant(int num)
+        {
+
+
+            //temporary to avoid errors
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
